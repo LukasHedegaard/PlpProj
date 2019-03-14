45b9d0347bb262bfba33e21ae1e74c0d4b4531af
HootERasmus <erasmus1212@hotmail.com>
2019-03-14 16:32:46 +0100

    use mapc to flip

--- src/line.scala
+++ src/line.scala
@@ -1,54 +1,55 @@
-class Point(xc: Int, yc: Int) {
-    var x: Int = xc
-    var y: Int = yc
+class Point(val x: Int, val y: Int)    
+
+def flip(point: Point): Point = {
+    return new Point(point.y, point.x)
 }
 
+def mapc(list: List[Point], func: Point => Point, cont: List[Point] => List[Point]): List[Point] = list match {
+    case Nil => cont(Nil)
+    case s :: rest => mapc(rest, func, x => cont( func(s) :: x))
+}
 
-class PointList
-    case class Cons(x: Int, y: Int, tl: PointList) extends PointList
-    case class Nil() extends PointList    
-
-var list = Cons(1,1, Cons(1,2, Cons(1,3, Nil())));
-
-def line(x1: Int, y1: Int, x2: Int, y2: Int) {
-    var dx = x2 - x1
-    var dy = y2 - y1
-    var d = 2*dy - dx
-    var y = y1
-
-    for (x <- x1 to x2) {
-        println("x:" + x + " y:" + y)
-        if (d > 0) {
-            y = y + 1
-            d = d - 2*dx
-        }
-        d = d + 2*dy
+def lineR(x1: Int, y1: Int, x2: Int, y2: Int, dx: Int, dy: Int, d: Int, list: List[Point]): List[Point] = {
+    
+    if(x1 == x2) {        
+        return new Point(x1, y2) :: list
+    } else {      
+        val myList = new Point(x1, y1) :: list
+                
+        if(d > 0) {
+            lineR(x1 + 1, y1 + 1, x2, y2, dx, dy, d - 2*dx + 2*dy, myList)
+        } else {
+            lineR(x1 + 1, y1, x2, y2, dx, dy, d + 2*dy, myList)
+        }     
     }
 }
 
-def lineStart(x1: Int, y1: Int, x2: Int, y2: Int) {
-    var dx = x2 - x1
-    var dy = y2 - y1
-    var d = 2*dy - dx
-    
-    lineR(x1, y1, x2, y2, dx, dy, d)
-}
-def lineR(x1: Int, y1: Int, x2: Int, y2: Int, dx: Int, dy: Int, d: Int) {
-    var newY = y1
-    var newD = d
-    
-    if(x1 == x2) {
-        println("x:" + x1 + " y:" + y2)
+def lineStart(x1: Int, y1: Int, x2: Int, y2: Int): List[Point] = {
+     
+    if(y2 > x2) {
+        val dx = y2 - x1
+        val dy = x2 - y1
+        val d = 2*dy - dx
+
+        val list = lineR(x1, y1, y2, x2, dx, dy, d, List[Point]())
+        return mapc(list, flip, x => x)
     } else {
-        println("x:" + x1 + " y:" + newY)
-        if (d > 0) {
-            newY = y1 + 1
-            newD = d - 2*dx
-        }
-        newD = newD + 2*dy
+        val dx = x2 - x1
+        val dy = y2 - y1
+        val d = 2*dy - dx
 
-        lineR(x1 + 1, newY, x2, y2, dx, dy, newD)
+        return lineR(x1, y1, x2, y2, dx, dy, d, List[Point]())
+    } 
+}
+
+def print(list: List[Point]) {
+    for (point <- list) {
+        println("x:" + point.x + " y:" + point.y)
     }
 }
-line(1,1, 15,3)
-lineStart(1,1, 15,3)
+
+val line1 = lineStart(1, 1, 15, 3)
+val line2 = lineStart(1, 1, 3, 15)
+
+print(line1)
+print(line2)
